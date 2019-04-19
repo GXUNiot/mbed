39634be44c4d5c62883b0177c138952029550b0d
蒙良桥 <15977877715@qq.com>
2019-04-19 16:16:56 +0800

    增加数据类型

---
+++ platform/onenet/typedef.h
@@ -0,0 +1,14 @@
+#ifndef _TYPEDEF_H
+#define _TYPEDEF_H
+
+typedef signed   char       int8_t;
+typedef unsigned char       uint8_t;
+//typedef signed   int        int16_t;
+//typedef unsigned int        uint16_t;
+//typedef signed   long       int32_t;
+//typedef unsigned long       uint32_t;
+typedef signed   long long  int64_t;
+typedef unsigned long long  uint64_t;
+
+
+#endif //_TYPEDEF_H
